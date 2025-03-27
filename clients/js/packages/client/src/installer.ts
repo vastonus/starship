752261754f7fc3666b36423d9e991fad2b733472
Anmol1696 <anmol1696@gmail.com>
2025-03-27 18:27:10 +0530

    add an end statement to the install command

--- clients/js/packages/client/src/installer.ts
+++ clients/js/packages/client/src/installer.ts
@@ -42,9 +42,10 @@ export class StarshipInstaller {
 
   async checkAndInstallDependencies() {
     for (const dependency of Object.keys(this.installations)) {
-      console.log(`Checking ${dependency}...`);
+      console.log(chalk.yellow(`Checking ${dependency}...`));
       await this.checkAndInstallBinary(dependency);
     }
+    console.log(chalk.green('All dependencies are installed!! Good to go!!'));
   }
 
   async installBinary(binaryName: string) {
@@ -61,7 +62,3 @@ export class StarshipInstaller {
     shell.exec(command);
   }
 }
-
-// Example usage:
-// const client = new StarshipClient();
-// await client.checkAndInstallBinary('kubectl');
