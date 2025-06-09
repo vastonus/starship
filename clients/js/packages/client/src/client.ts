457ee56df69b99f2018f8bd268026bef77b603c3
Anmol1696 <anmol1696@gmail.com>
2025-06-09 17:35:24 +0900

    set default version of chart to latest

--- clients/js/packages/client/src/client.ts
+++ clients/js/packages/client/src/client.ts
@@ -52,7 +52,7 @@ export interface PodPorts {
 }
 
 const defaultName: string = 'starship';
-const defaultVersion: string = 'v1.6.0';
+const defaultVersion: string = 'v1.8.0';
 
 // TODO talk to Anmol about moving these into yaml, if not already possible?
 const defaultPorts: PodPorts = {
