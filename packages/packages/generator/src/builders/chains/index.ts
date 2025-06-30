43d5af182c3395493e6995f701923c67b7dc4c81
Anmol1696 <anmol1696@gmail.com>
2025-06-30 17:57:57 +0530

    use chain builder as a router for all chains to be supported

---
+++ packages/packages/generator/src/builders/chains/index.ts
@@ -0,0 +1,40 @@
+import { Chain, StarshipConfig } from '@starship-ci/types';
+
+import { IGenerator, Manifest } from '../../types';
+import { CosmosBuilder } from './cosmos';
+
+// Export all individual builders and components
+export * from './cosmos';
+
+const chainBuilderRegistry: Record<
+  string,
+  new (config: StarshipConfig) => IGenerator
+> = {
+  // ethereum: EthereumBuilder, // Future: when ethereum builder is implemented
+};
+
+function createBuilder(chainName: string, config: StarshipConfig): IGenerator {
+  const builder = chainBuilderRegistry[chainName] || CosmosBuilder;  // default to cosmos builder if no builder is found
+  return new builder(config);
+}
+
+/**
+ * Main ChainBuilder that uses the factory pattern to create appropriate builders
+ */
+export class ChainBuilder implements IGenerator {
+  private config: StarshipConfig;
+  private generators: IGenerator[] = [];
+
+  constructor(config: StarshipConfig) {
+    this.config = config;
+    
+    // Create builders for each chain
+    this.config.chains?.forEach((chain) => {
+      this.generators.push(createBuilder(chain.name, this.config));
+    });
+  }
+
+  generate(): Manifest[] {
+    return this.generators.flatMap((generator) => generator.generate());
+  }
+}
--- packages/packages/generator/src/builders/relayers/index.ts
+++ packages/packages/generator/src/builders/relayers/index.ts
@@ -38,17 +38,13 @@ function createBuilder(relayer: Relayer, config: StarshipConfig): IGenerator {
 export class RelayerBuilder implements IGenerator {
   private config: StarshipConfig;
   private relayers: Relayer[];
-  private defaultsManager: DefaultsManager;
   private generators: IGenerator[] = [];
 
   constructor(config: StarshipConfig) {
     this.config = config;
-    this.defaultsManager = new DefaultsManager();
 
     // Process relayers with defaults
-    this.relayers = (config.relayers || []).map((relayer) =>
-      this.defaultsManager.processRelayer(relayer)
-    );
+    this.relayers = config.relayers || []
 
     this.generators = this.relayers.map((relayer) =>
       createBuilder(relayer, this.config)
