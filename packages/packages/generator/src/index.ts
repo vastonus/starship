03cf6829340a3f930dbc54ef9c8fdf44a6600869
Anmol1696 <anmol1696@gmail.com>
2025-06-30 22:55:56 +0530

    remove cosmos builder export to chain builder export

--- packages/packages/generator/src/index.ts
+++ packages/packages/generator/src/index.ts
@@ -5,8 +5,5 @@ export * from './types';
 export { DefaultsManager } from './defaults';
 export { ScriptManager } from './scripts';
 
-// Builders
-export { CosmosBuilder } from './builders/cosmos';
-
-// Re-export types from starship-ci/types for convenience
-export { Chain, StarshipConfig } from '@starship-ci/types';
+// Chain builders
+export { ChainBuilder } from './builders/chains';
