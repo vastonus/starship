a08dc48f9dddaa8a2f561cd09706caeaf6ace86d
Anmol1696 <anmol1696@gmail.com>
2025-07-18 20:15:23 +0530

    update services and faucet, ws and p2p ports

--- starship/charts/devnet/scripts/solana/validator-x.sh
+++ starship/charts/devnet/scripts/solana/validator-x.sh
@@ -1,21 +1,7 @@
 #!/usr/bin/env bash
+#
+# Start a dynamically-configured validator
+#
 
 here=$(dirname "$0")
-# shellcheck source=multinode-demo/common.sh
-source "$here"/common.sh
-
-# shellcheck source=net/common.sh
-source "$(cd "$here"/..; pwd)"/net/common.sh
-
-if [[ -z $1 ]]; then
-  #
-  # Start a validator that connects to a bootstrap validator
-  #
-  $agave_validator_cuda \
-    --contact-info 127.0.0.1:8001 \
-    "$@"
-else
-  $agave_validator \
-    --contact-info 127.0.0.1:8001 \
-    "$@"
-fi 
+exec "$here"/validator.sh --label x$$ "$@"
