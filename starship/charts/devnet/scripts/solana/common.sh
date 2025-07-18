fb9ed4aee24e9507dae81f78d9441f819fc720d6
Anmol1696 <anmol1696@gmail.com>
2025-07-18 20:39:17 +0530

    update images and scripts to use agave

--- starship/charts/devnet/scripts/solana/common.sh
+++ starship/charts/devnet/scripts/solana/common.sh
@@ -27,7 +27,7 @@ solana_program() {
     printf "solana"
   else
     if [[ $program == "validator" || $program == "ledger-tool" || $program == "watchtower" || $program == "install" ]]; then
-      printf "solana-%s" "$program"
+      printf "agave-%s" "$program"
     else
       printf "solana-%s" "$program"
     fi
