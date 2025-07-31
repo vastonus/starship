ac71f51362f13bffa002b7a9d6f55bd8a9f2f26c
Anmol1696 <anmol1696@gmail.com>
2025-07-31 17:07:47 +0530

    add solana-faucet to faucet types

--- starship/charts/devnet/scripts/solana/faucet.sh
+++ starship/charts/devnet/scripts/solana/faucet.sh
@@ -16,4 +16,10 @@ source "$here"/common.sh
 
 set -x
 # shellcheck disable=SC2086 # Don't want to double quote $solana_faucet
-exec $solana_faucet --keypair "$SOLANA_CONFIG_DIR"/faucet.json "$@"
+exec $solana_faucet \
+  --keypair "$SOLANA_CONFIG_DIR"/faucet.json \
+  --port 9999 \
+  --allow-ip 0.0.0.0/0 \
+  --per-time-cap 999999999 \
+  --per-request-cap 999999999 \
+  "$@"
